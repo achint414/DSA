a9c833b723de7b29acbf315214e68aaca32c8b96
achint414 <achint414@gmail.com>
2023-08-16 17:05:39 +0530

    update

--- Practice/2D Array/transposeOfMatrix.cpp
+++ Practice/2D Array/transposeOfMatrix.cpp
@@ -33,3 +33,40 @@ for(int i=0;i<N;i++){
 }
     return 0;
 }
+
+//brute
+// #include<iostream>
+// #define N 3
+// using namespace std;
+
+// void transpose(int arr[][N], int tpose[][N]){
+// int temp=0;
+// for(int i=0;i<N;i++){
+//     for(int j=i;j<N;j++){
+//          tpose[j][i] == arr[i][j];
+//     }
+
+// }
+
+// }
+
+// int main(){
+
+// int arr[N][N];
+// for(int i=0;i<N;i++){
+//     for(int j=0;j<N;j++){
+//         cin>>arr[i][j];
+//     }
+// }
+
+// int tpose[N][N];
+
+// transpose(arr, tpose);
+
+// for(int i=0;i<N;i++){
+//     for(int j=0;j<N;j++){
+//         cout<<arr[i][j]<<" ";
+//     }
+// }
+//     return 0;
+// }
